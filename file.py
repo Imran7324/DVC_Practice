43b7f3605c27387829aa82e0d5b3b960ababc328
Imran_Sid <imransid7755@gmail.com>
2026-04-24 19:41:02 +0530

    Third Version of data saved

--- file.py
+++ file.py
@@ -12,7 +12,13 @@ new={
     "Age":26,
     "City":"Noida"
 }
+new1={
+    "Name":"Nina",
+    "Age":28,
+    "City":"Gurgaon"
+}
 df.loc[len(df.index)]=new
+df.loc[len(df.index)]=new1
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'sample_data.csv')
