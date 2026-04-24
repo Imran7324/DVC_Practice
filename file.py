5569fc70104a694a0fd1d9158ebcb234bb5f6955
Imran_Sid <imransid7755@gmail.com>
2026-04-24 18:32:41 +0530

    DVC INITIATED with Data Version1

--- file.py
+++ file.py
@@ -10,4 +10,4 @@ data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'sample_data.csv')
 df.to_csv(file_path,index=False)
-print(f"CSV file saved to {file_path}")
+print(f"CSV file saved to {file_path}") 
