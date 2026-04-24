bdcd3d2d92ea52346994104896a3836ce9ed7168
Imran_Sid <imransid7755@gmail.com>
2026-04-24 18:03:44 +0530

    Initial Commit

---
+++ file.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+data={
+    "Name":['Max','Mac','Matt'],
+    "Age":[24,25,26],
+    "City":["Delhi","Nagpur","Bhopal"]
+}
+df=pd.DataFrame(data)
+data_dir='data'
+os.makedirs(data_dir,exist_ok=True)
+file_path=os.path.join(data_dir,'sample_data.csv')
+df.to_csv(file_path,index=False)
+print(f"CSV file saved to {file_path}")
